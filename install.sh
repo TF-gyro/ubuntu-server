510b9cfdd37474e74da4d79984d0549393ced4eb
cityowl / moti <cityowl@hey.com>
2024-06-27 02:41:26 +0530

    install path

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-installpath="~/ubuntu-server";
+installpath="ubuntu-server";
 read -p "MySQL Root Username: " mysqluser;
 echo "MySQL Root Password:";
 read -s mysqlpass;
@@ -6,5 +6,5 @@ git clone https://github.com/tribe-framework/ubuntu-server.git $installpath;
 sed -i "s/mysql_root_user/$mysqluser/g" $installpath/server.sh;
 sed -i "s/mysql_root_pass/$mysqlpass/g" $installpath/server.sh;
 bash $installpath/server.sh;
-rm $installpath/server.sh;
-rm $installpath/install.sh;
+rm $installpath -R;
+rm install.sh;
