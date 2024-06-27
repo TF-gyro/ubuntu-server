558dc143573f8bf4e7b6537f4721ac693128601d
cityowl / jigmet <cityowl@hey.com>
2024-06-27 17:06:12 +0530

    noninteractive

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-DEBIAN_FRONTEND=noninteractive apt-get -yq update
-DEBIAN_FRONTEND=noninteractive apt-get -yq upgrade
-DEBIAN_FRONTEND=noninteractive apt-get -yq install swapspace lsb-release ca-certificates curl apt-transport-https software-properties-common nginx python3-pip python3-certbot-nginx net-tools zip unzip p7zip-full build-essential curl s3cmd htop imagemagick ffmpeg poppler-utils
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -yq update
+apt-get -yq upgrade
+apt-get -yq install swapspace lsb-release ca-certificates curl apt-transport-https software-properties-common nginx python3-pip python3-certbot-nginx net-tools zip unzip p7zip-full build-essential curl s3cmd htop imagemagick ffmpeg poppler-utils
 ufw allow OpenSSH
 ufw allow Postfix
 ufw allow 80
@@ -16,4 +18,4 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-DEBIAN_FRONTEND=noninteractive apt-get -yq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get -yq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
