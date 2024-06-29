191a153fc401891af3745d5fa9f931020fc5f466
cityowl / jigmet <cityowl@hey.com>
2024-06-29 15:59:13 +0530

    php

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -yq update
 apt-get -yq upgrade
 
-apt-get -yq install swapspace lsb-release ca-certificates curl apt-transport-https software-properties-common nginx python3-pip python3-certbot-nginx net-tools zip unzip p7zip-full build-essential curl s3cmd htop imagemagick ffmpeg poppler-utils
+apt-get -yq install swapspace lsb-release ca-certificates curl php php-cgi apt-transport-https software-properties-common nginx python3-pip python3-certbot-nginx net-tools zip unzip p7zip-full build-essential curl s3cmd htop imagemagick ffmpeg poppler-utils
 
 ufw allow OpenSSH
 ufw allow Postfix
