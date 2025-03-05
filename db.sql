8d748f879c6cf21981a424fe3b1a63522a0c9790
Apurv Jyotirmay <mail@jyotirmay.dev>
2025-03-06 03:17:38 +0530

    added new tables for process spawns

--- db.sql
+++ db.sql
@@ -1,44 +1,139 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- MySQL dump 10.13  Distrib 9.2.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: gyro
+-- ------------------------------------------------------
+-- Server version	9.2.0
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `data` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `content` json DEFAULT NULL,
-  `updated_on` bigint(20) DEFAULT NULL,
-  `created_on` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_on` bigint DEFAULT NULL,
+  `created_on` bigint NOT NULL,
+  `user_id` varchar(6) GENERATED ALWAYS AS (json_unquote(json_extract(`content`,_utf8mb3'$.user_id'))) VIRTUAL,
+  `role_slug` varchar(100) GENERATED ALWAYS AS (json_unquote(json_extract(`content`,_utf8mb3'$.role_slug'))) VIRTUAL,
+  `slug` varchar(255) GENERATED ALWAYS AS (json_unquote(json_extract(`content`,_utf8mb3'$.slug'))) VIRTUAL,
+  `content_privacy` varchar(100) GENERATED ALWAYS AS (json_unquote(json_extract(`content`,_utf8mb3'$.content_privacy'))) VIRTUAL,
+  `type` varchar(100) GENERATED ALWAYS AS (json_unquote(json_extract(`content`,_utf8mb3'$.type'))) VIRTUAL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `slug` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE `data`
-  ADD INDEX(`id`);
+--
+-- Dumping data for table `data`
+--
 
-ALTER TABLE `data`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `data` WRITE;
+/*!40000 ALTER TABLE `data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE `data`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- Table structure for table `dockers`
+--
 
-ALTER TABLE `data` ADD `user_id` VARCHAR(6) AS (`content`->>'$.user_id') VIRTUAL NULL AFTER `created_on`, ADD INDEX (`user_id`);
-ALTER TABLE `data` ADD `role_slug` VARCHAR(100) AS (`content`->>'$.role_slug') VIRTUAL NULL AFTER `user_id`;
-ALTER TABLE `data` ADD `slug` VARCHAR(255) AS (`content`->>'$.slug') VIRTUAL NULL AFTER `role_slug`, ADD INDEX (`slug`);
-ALTER TABLE `data` ADD `content_privacy` VARCHAR(100) AS (`content`->>'$.content_privacy') VIRTUAL NULL AFTER `slug`;
-ALTER TABLE `data` ADD `type` VARCHAR(100) AS (`content`->>'$.type') VIRTUAL NULL AFTER `content_privacy`;
+DROP TABLE IF EXISTS `dockers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dockers` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(100) NOT NULL,
+  `app_name` varchar(100) NOT NULL,
+  `tribe_port` int unsigned NOT NULL,
+  `junction_port` int unsigned NOT NULL,
+  `status` enum('pending','success','failed') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `dockers`
+--
+
+LOCK TABLES `dockers` WRITE;
+/*!40000 ALTER TABLE `dockers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dockers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job_logs`
+--
+
+DROP TABLE IF EXISTS `job_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job_logs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `job_id` varchar(255) DEFAULT NULL,
+  `status` enum('pending','running','completed','failed') DEFAULT NULL,
+  `output` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job_logs`
+--
+
+LOCK TABLES `job_logs` WRITE;
+/*!40000 ALTER TABLE `job_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `meta`
+--
+
+DROP TABLE IF EXISTS `meta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `meta` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `content` json DEFAULT NULL,
-  `updated_on` bigint(20) DEFAULT NULL,
-  `created_on` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_on` bigint DEFAULT NULL,
+  `created_on` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE `meta`
-  ADD INDEX(`id`);
+--
+-- Dumping data for table `meta`
+--
 
-ALTER TABLE `meta`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `meta` WRITE;
+/*!40000 ALTER TABLE `meta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `meta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-ALTER TABLE `meta`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-COMMIT;
+-- Dump completed on 2025-03-05 20:09:26
