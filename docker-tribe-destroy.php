34d7699bb3d8acb00b70b613211a9570bf740b62
Apurv Jyotirmay <mail@jyotirmay.dev>
2025-05-16 16:54:33 +0530

    fix: handle huge data passed to API

--- docker-tribe-destroy.php
+++ docker-tribe-destroy.php
@@ -1,5 +1,10 @@
 <?php
 $_POST = file_get_contents("php://input");
-parse_str($_POST, $or);
-shell_exec("echo ".$or['app_uid']." >> /var/www/html/docker-tribe-destroy-slugs.txt");
+
+$slug = explode("&", $_POST);
+$slug = array_pop($slug);
+
+parse_str($slug, $or);
+
+shell_exec("echo {$or['app_uid']} >> /var/www/html/docker-tribe-destroy-slugs.txt");
 ?>
